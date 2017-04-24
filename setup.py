54f8e9082fc93eeacf9d731919582df0b0f0367a
Sergey Gornostaev <sergey@gornostaev.su>
2017-04-24 18:25:19 +0800

    Косметические исправления в настройках репозитория и сборки

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup
 
 try:
-    long_description = open('README.rst').read().decode('utf-8') + u'\n\n' + open('CHANGES.rst').read().decode('utf-8')
-except UnicodeDecodeError:
     long_description = open('README.rst', encoding='utf-8').read() + u'\n\n' + open('CHANGES.rst', encoding='utf-8').read()
+except TypeError:
+    long_description = open('README.rst').read().decode('utf-8') + u'\n\n' + open('CHANGES.rst').read().decode('utf-8')
 
 setup(
     name='django-ckeditor-filer',
@@ -17,13 +17,13 @@ setup(
 
     url='https://bitbucket.org/TheDeadOne/django-ckeditor-filer/',
     license = 'BSD license',
-    description = u'Кнопка для django-ckeditor, позволяющая загружать изображения в django-filer.',
+    description = u'Adds a button in the django-ckeditor to load images into the django-filer',
     long_description = long_description,
 
     install_requires=['django-filer', 'django-ckeditor'],
 
     classifiers=(
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
